d4efab0e2d50b2b204c48f275054cca4b9c7f44b
Pino <1473781785@qq.com>
2018-04-18 12:36:18 +0800

    first

---
+++ Owen/MyTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewCell.h
+//  Owen
+//
+//  Created by  P1no on 2018/4/18.
+//  Copyright © 2018年  P1no. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewCell : UITableViewCell
+
+@end
---
+++ Owen/MyTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.m
+//  Owen
+//
+//  Created by  P1no on 2018/4/18.
+//  Copyright © 2018年  P1no. All rights reserved.
+//
+
+#import "MyTableViewCell.h"
+
+@implementation MyTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
